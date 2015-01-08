5ffeee29e5dbc3bbc2e7552936d061e4dbe7d4d6
John Wu <john@iMac-de-John.local>
2015-01-08 20:06:11 +0000

    Prepare for heroku

--- Gemfile
+++ Gemfile
@@ -1,21 +1,23 @@
 source 'https://rubygems.org'
-ruby '2.1.1'
+ruby '2.1.5'
 
 gem 'byebug', group: :development
+gem 'coffee-rails', '~> 4.0.0'
 gem 'country_select', github: 'stefanpenner/country_select'
 gem 'devise', '~> 3.4.1'
 gem 'foundation-rails'
 gem 'foundation-icons-sass-rails'
 gem 'haml'
 gem 'httparty'
-gem 'rails', '4.1.8'
-gem 'simple_form'
-gem 'sqlite3', group: :development
-gem 'sass-rails', '~> 5.0.0'
-gem 'uglifier', '>= 1.3.0'
-gem 'coffee-rails', '~> 4.0.0'
 gem 'jquery-rails'
 gem 'jwt'
-gem 'turbolinks'
+gem 'pg', group: :production
+gem 'rails', '4.1.8'
+gem 'rails_12factor', group: :production
+gem 'sass-rails', '~> 5.0.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
+gem 'simple_form'
 gem 'spring', group: :development
+gem 'sqlite3', group: :development
+gem 'turbolinks'
+gem 'uglifier', '>= 1.3.0'
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150105141323) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -46,10 +49,10 @@ ActiveRecord::Schema.define(version: 20150105141323) do
     t.string   "vat_number"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["quaderno_account_id"], name: "index_users_on_quaderno_account_id"
-  add_index "users", ["quaderno_id"], name: "index_users_on_quaderno_id"
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  add_index "users", ["subscription_id"], name: "index_users_on_subscription_id"
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["quaderno_account_id"], name: "index_users_on_quaderno_account_id", using: :btree
+  add_index "users", ["quaderno_id"], name: "index_users_on_quaderno_id", using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["subscription_id"], name: "index_users_on_subscription_id", using: :btree
 
 end
